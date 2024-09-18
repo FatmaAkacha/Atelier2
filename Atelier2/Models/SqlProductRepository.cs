1f31f87fd01e9c8574db25b7b8b38ce4be7dd725
FatmaAkacha <akachafatma1@gmail.com>
2024-09-18 20:10:15 +0100

    Ajoutez des fichiers projet.

---
+++ Atelier2/Models/SqlProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Atelier2.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atelier2.Models
+{
+    public class SqlProductRepository : IRepository<Product>
+    {
+        private readonly AppDbContext context;
+        public SqlProductRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public Product Add(Product P)
+        {
+            context.Products.Add(P);
+            context.SaveChanges();
+            return P;
+        }
+        public Product Delete(int Id)
+        {
+            Product P = context.Products.Find(Id);
+            if (P != null)
+            {
+                context.Products.Remove(P);
+                context.SaveChanges();
+            }
+            return P;
+        }
+
+        public Product Get(int Id)
+        {
+            return context.Products.Find(Id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return context.Products;
+        }
+
+        public Product Update(Product t)
+        {
+            var Product =
+         context.Products.Attach(t);
+            Product.State = EntityState.Modified;
+            context.SaveChanges();
+            return t;
+        }
+    }
+   
+   
+}
---
+++ Atelier2/ViewModels/EditViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Atelier2.ViewModels
+{
+    public class EditViewModel : CreateViewModel
+    {
+        public int Id { get; set; }
+        public string ExistingImagePath { get; set; }
+    }
+}
